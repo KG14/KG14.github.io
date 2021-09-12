9c51457af253040af612d98c6316c441c96423e4
Karan Gupta <tokarangupta@gmail.com>
2021-09-12 13:36:10 -0700

    Added gradient color scheme, our impact section

--- js/index.js
+++ js/index.js
@@ -1,7 +1,19 @@
+// Total number of quotes
+let totalQuotes = 4;
+
+// Index of current quote
+var currentQuote = 0;
+
 document.addEventListener('DOMContentLoaded', function() {
-   
+
+    let leftQuoteButton = document.querySelector('#left-quote-btn');
+    let rightQuoteButton = document.querySelector('#right-quote-btn');
+
+    let carousel = document.querySelector('#quotes-carousel');
+    let quotes = Array.from(carousel.children);
+
     // Get "navbar-burger" element
-    burger = document.querySelector('.navbar-burger')
+    let burger = document.querySelector('.navbar-burger')
 
     burger.addEventListener('click', () => {
 
@@ -9,6 +21,28 @@ document.addEventListener('DOMContentLoaded', function() {
         burger.classList.toggle('is-active')
         document.querySelector('.navbar-menu').classList.toggle('is-active');
 
+    })
+
+    leftQuoteButton.addEventListener('click', () => {
+        if (currentQuote == 0) {
+            currentQuote = totalQuotes - 1
+            scrollCarousel(`#${quotes[currentQuote].id}`)
+        }
+        else {
+            currentQuote -= 1;
+            scrollCarousel(`#${quotes[currentQuote].id}`)
+        }
+    })
+
+    rightQuoteButton.addEventListener('click', () => {
+        if (currentQuote == totalQuotes - 1) {
+            currentQuote = 0
+            scrollCarousel(`#${quotes[currentQuote].id}`)
+        }
+        else {
+            currentQuote += 1;
+            scrollCarousel(`#${quotes[currentQuote].id}`)
+        }
     })
 });
 
@@ -28,4 +62,27 @@ function showInfo(icon) {
 
 function scrollSection(id, position) {
     document.querySelector(id).scrollIntoView(position);
+}
+
+function scrollCarousel(id) {
+    console.log(typeof(id))
+    document.querySelector(id).scrollIntoView({behavior: 'smooth', block: 'nearest', inline: 'start'});
+    console.log("ran")
+}
+
+function continuousCarouselScroll() {
+    carousel = document.querySelector('#quotes-carousel');
+    quotes = Array.from(carousel.children);
+
+    index = 0;
+    while (true) {
+        if (index > 2) {
+            index = 0
+        }
+
+        
+        setTimeout(function() {
+            scrollCarousel(`#${quotes[index].id}`);
+        }, 2);
+    }
 }
